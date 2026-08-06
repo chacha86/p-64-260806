082e3ab0f401de28da4bfe91291f8ad68a244893
chacha86 <taejincha0619@gmail.com>
2026-08-06 12:20:45 +0900

    13

--- src/main/java/com/back/p64260806/PersonController.java
+++ src/main/java/com/back/p64260806/PersonController.java
@@ -14,6 +14,7 @@ public class PersonController {
     @GetMapping("/people")
     @ResponseBody
     public String people() {
+        System.out.println("프록시 테스트");
         return "사람 수 : %d".formatted(personService.count());
     }
 
--- src/main/java/com/back/p64260806/PersonService.java
+++ src/main/java/com/back/p64260806/PersonService.java
@@ -1,10 +1,12 @@
 package com.back.p64260806;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
 
+    @Transactional
     public int count() {
         return 3;
     }
