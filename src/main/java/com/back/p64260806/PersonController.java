87adad12957c3bf66c7879d66f7fd2503e347bc9
chacha86 <taejincha0619@gmail.com>
2026-08-06 11:04:58 +0900

    2

---
+++ src/main/java/com/back/p64260806/PersonController.java
@@ -0,0 +1,18 @@
+package com.back.p64260806;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PersonController {
+
+    private PersonService personService = new PersonService();
+
+    @GetMapping("/people")
+    @ResponseBody
+    public String people() {
+        return "사람 수 : %d".formatted(personService.count());
+    }
+
+}
