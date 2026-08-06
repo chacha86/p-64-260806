bcb1a76a0d42276d2706d557c0a878b185f568d5
chacha86 <taejincha0619@gmail.com>
2026-08-06 12:22:33 +0900

    13-restore

--- src/main/java/com/back/p64260806/PersonService.java
+++ src/main/java/com/back/p64260806/PersonService.java
@@ -1,12 +1,10 @@
 package com.back.p64260806;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
 
-    @Transactional
     public int count() {
         return 3;
     }
