a13c2b9110aa5800e101cb45d3e24c40630cbdc3
chacha86 <taejincha0619@gmail.com>
2026-08-07 12:12:28 +0900

    22

--- src/main/java/com/back/p64260806/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/p64260806/domain/wiseSaying/entity/WiseSaying.java
@@ -14,6 +14,11 @@ public class WiseSaying {
     private String content;
     private String author;
 
+    public WiseSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
     public void update(String content, String author) {
         this.content = content;
         this.author = author;
