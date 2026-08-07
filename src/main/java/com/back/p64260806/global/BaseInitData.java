7e419f5e31c405c47ad9196d2cda256cd83daf06
chacha86 <taejincha0619@gmail.com>
2026-08-07 12:22:34 +0900

    23

--- src/main/java/com/back/p64260806/global/BaseInitData.java
+++ src/main/java/com/back/p64260806/global/BaseInitData.java
@@ -2,6 +2,7 @@ package com.back.p64260806.global;
 
 import com.back.p64260806.domain.member.entity.Member;
 import com.back.p64260806.domain.member.service.MemberService;
+import com.back.p64260806.domain.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -19,11 +20,13 @@ public class BaseInitData {
     private BaseInitData self; // proxy 리모컨
 
     private final MemberService memberService;
+    private final WiseSayingService wiseSayingService;
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
             self.work1();
             self.work2();
+            work3();
         };
 
     }
@@ -49,5 +52,18 @@ public class BaseInitData {
         System.out.println("work2 수행");
         Member member4 = memberService.findByUsername("user2").get();
         member4.setNickname("new user2"); // 더티체킹에 의해 트랜잭션 종료후 DB 반영
+    }
+
+    void work3() {
+
+        if(wiseSayingService.count() > 0) {
+            return;
+        }
+
+        wiseSayingService.write("명언1", "작가1");
+        wiseSayingService.write("명언2", "작가2");
+        wiseSayingService.write("명언3", "작가3");
+        wiseSayingService.write("명언4", "작가4");
+        wiseSayingService.write("명언5", "작가5");
     }
 }
