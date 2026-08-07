11da62d713bcd21d4c30e77a0b51d2a21f4e5ce6
chacha86 <taejincha0619@gmail.com>
2026-08-07 10:10:27 +0900

    17-tmp2

--- src/main/java/com/back/p64260806/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/p64260806/domain/wiseSaying/controller/WiseSayingController.java
@@ -2,7 +2,13 @@ package com.back.p64260806.domain.wiseSaying.controller;
 
 import com.back.p64260806.domain.wiseSaying.entity.WiseSaying;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WiseSayingController {
+
+    private List<WiseSaying> wiseSayings = new ArrayList<>();
+    private int lastId = 0;
 
     public void actionAdd() {
         System.out.print("명언 : ");
@@ -10,7 +16,9 @@ public class WiseSayingController {
         System.out.print("작가 : ");
         String author = sc.nextLine();
 
-        WiseSaying wiseSaying = wiseSayingService.write(saying, author);
+        WiseSaying wiseSaying = new WiseSaying(saying, author);
+        wiseSaying.setId(++lastId);
+        wiseSayings.add(wiseSaying);
 
         System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
--- src/main/java/com/back/p64260806/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/p64260806/domain/wiseSaying/entity/WiseSaying.java
@@ -4,6 +4,7 @@ import com.back.p64260806.global.BaseEntity;
 import jakarta.persistence.Entity;
 import lombok.*;
 
+@Setter
 @Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
 @NoArgsConstructor // 매개변수 없는 기본 생성자 => 외부 라이브러리
 @AllArgsConstructor // 모든 인스턴스 변수에 대한 매개변수를 받는 생성자
